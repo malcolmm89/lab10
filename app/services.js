4481754b4ebe9032831362f88c31b7812d32baac
malcolmm89 <71410450+malcolmm89@users.noreply.github.com>
2020-12-20 16:53:47 -0500

    Type and Update working

--- app/services.js
+++ app/services.js
@@ -65,6 +65,32 @@ var services = function (app) {
         });
     });  //end read
 
+    app.get('/read-type', function (req, res) {
+        var type = req.query.publisher;
+        var search = (type == "") ? {} : { publisher: type };
+        var sortBy = { gameTitle: 1 };
+
+        MongoClient.connect(dbURL, { useUnifiedTopology: true }, function (err, client) {
+            if (err) {
+                return res.status(200).send(JSON.stringify({ msg: "error " + err }));
+            }
+            else {
+                var dbo = client.db("gameDB");
+
+                dbo.collection("games").find(search).sort(sortBy).toArray(function (err, data) {
+                    if (err) {
+                        client.close();
+                        return res.status(200).send(JSON.stringify({ msg: "error " + err }));
+                    }
+                    else {
+                        client.close();
+                        return res.status(200).send(JSON.stringify({ msg: "success", games: data }));
+                    }
+                })
+            }
+        })
+    }); // end of read-type
+
     app.delete('/delete-record', function (req, res) {
         var gameID = req.query.gameId;
 
@@ -78,7 +104,7 @@ var services = function (app) {
             else {
                 var dbo = client.db("gameDB");
 
-                dbo.collection("games").deleteOne(search, function (err, data) {
+                dbo.collection("games").deleteOne(search, function (err) {
                     if (err) {
                         client.close();
                         return res.status(200).send(JSON.stringify({ msg: "error " + err }));
@@ -92,6 +118,47 @@ var services = function (app) {
         });
 
     });  //end of delete
+
+    app.put('/update-game', function (req, res) {
+        var gameID = req.body.gameId;
+        var gameTitle = req.body.gameTitle;
+        var developer = req.body.developer;
+        var publisher = req.body.publisher;
+        var releaseDate = req.body.releaseDate;
+        var score = req.body.score;
+
+        var g_id = new ObjectID(gameID);
+        var search = { _id: g_id };
+        var updateData = {
+            $set: {
+                gameTilte: gameTitle,
+                developer: developer,
+                publisher: publisher,
+                releaseDate: releaseDate,
+                score: score
+            }
+        }
+
+        MongoClient.connect(dbURL, { useUnifiedTopology: true }, function (err, client) {
+            if (err) {
+                return res.status(200).send(JSON.stringify({ msg: "error " + err }));
+            }
+            else {
+                var dbo = client.db("gameDB");
+
+                dbo.collection("games").updateOne(search, updateData, function (err) {
+                    if (err) {
+                        client.close();
+                        return res.status(200).send(JSON.stringify({ msg: "error " + err }));
+                    }
+                    else {
+                        client.close();
+                        return res.status(200).send(JSON.stringify({ msg: "success" }));
+                    }
+                });
+            }
+        });
+    });
 
 }  //end of sevices function
 
--- client/js/browse.js
+++ client/js/browse.js
@@ -2,6 +2,7 @@ var app = angular.module("tableApp", []);
 
 app.controller('tableCtrl', function ($scope, $http) {
     $scope.games = [];
+    $scope.publishers = [];
 
     $scope.getGames = function () {
         $http({
@@ -10,6 +11,8 @@ app.controller('tableCtrl', function ($scope, $http) {
         }).then(function (response) {
             if (response.data.msg == "success") {
                 $scope.games = response.data.games;
+                $scope.publishers = getPub(response.data.games);
+                $scope.selectedPublisher = $scope.publishers[0];
             }
             else {
                 console.log(response.data.games);
@@ -17,8 +20,28 @@ app.controller('tableCtrl', function ($scope, $http) {
         }, function (response) {
                 console.log(response);
         });
-    }
+    } //end of get
+    
+    $scope.redraw = function () {
+        var type = $scope.selectedPublisher.value;
 
+        $http({
+            method: "get",
+            url: gameURL + "/read-type",
+            params: { publisher: type }
+        }).then(function (response) {
+            if (response.data.msg == "success") {
+                $scope.games = response.data.games;
+                //$scope.getGames();
+            }
+            else {
+                console.log(response.data.msg);
+            }
+        }, function () {
+            console.log(response);
+        });
+    } // end of redraw
+    
     $scope.deleteGame = function (gameID) {
         console.log(gameID);
         $http({
@@ -27,7 +50,7 @@ app.controller('tableCtrl', function ($scope, $http) {
             params: { gameId: gameID }
         }).then(function (response) {
             if (response.data.msg == "success") {
-                $scope.getGames();
+                $scope.redraw();
             }
             else {
                 console.log(respones.data.msg);
@@ -35,11 +58,82 @@ app.controller('tableCtrl', function ($scope, $http) {
         }, function (response) {
                 console.log(response);
         });
+    } // end of delete
+
+    $scope.editGame = function (gameIndex) {
+        $scope.gameTitle = $scope.games[gameIndex].gameTitle;
+        $scope.developer = $scope.games[gameIndex].developer;
+        $scope.publisher = $scope.games[gameIndex].publisher;
+        $scope.releaseDate = $scope.games[gameIndex].releaseDate;
+        $scope.score = $scope.games[gameIndex].score;
+        $scope.gameID = $scope.games[gameIndex]['_id'];
+
+        $scope.hideTable = true;
+        $scope.hideForm = false;
+
+
+    }
+
+    $scope.updateGame = function () {
+        $http({
+            method: "put",
+            url: gameURL + "/update-game",
+            data: {
+                gameId: $scope.gameID,
+                gameTitle: $scope.gameTitle,
+                developer: $scope.developer,
+                publisher: $scope.publisher,
+                releaseDate: $scope.releaseDate,
+                score: $scope.score
+            }
+        }).then(function (response) {
+            if (response.data.msg == "success") {
+                $scope.hideTable = false;
+                $scope.hideForm = true;
+                $scope.redraw();
+
+                $scope.gameTitle = "";
+                $scope.developer = "";
+                $scope.publisher = "";
+                $scope.releaseDate = "";
+                $scope.score = "";
+                $scope.gameID = "";
+            }
+            else {
+                console.log(response.data.msg);
+            }
+        }, function () {
+                console.log(response);
+        });
     }
 
     $scope.getGames();
-});
 
+}); //end of app controller
+
+
+function getPub(pubData) {
+    //console.log(pubData);
+    var pubExists;
+    var pubArray = [{ value: "", display: "ALL" }];
+
+    for (var i = 0; i < pubData.length; i++) {
+        pubExists = pubArray.find(function (element) {
+            return element.value == pubData[i].publisher;
+        });
+
+        console.log("exist = " + pubExists);
+
+        if (pubExists) {
+            continue;
+        }
+        else {
+            pubArray.push({ value: pubData[i].publisher, display: pubData[i].publisher });
+        }
+    }
+    console.log(pubArray);
+    return pubArray;
+}
 
 ///////////////////////////////////////////////////////////////
 
